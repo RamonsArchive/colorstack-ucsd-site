26cb288fd45ebf0dc07ed93d2cae07d38f1703b4
RamonsArchive <rmcdeem.m@gmail.com>
2025-11-15 20:02:36 -0800

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,30 @@ export const metadata: Metadata = {
   title: "ColorStack @ UCSD",
   description:
     "Building a community of Black and Hispanic software engineers at UCSD.",
+  icons: {
+    icon: [
+      { url: "/icon.png", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        url: "/icon-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        url: "/icon-512x512.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
