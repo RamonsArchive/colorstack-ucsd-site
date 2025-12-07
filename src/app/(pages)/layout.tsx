7724cc1ce5b68dff493dbc88e6c4a219b0149871
RamonsArchive <rmcdeem.m@gmail.com>
2025-12-07 14:22:39 -0800

    added min-h-dvh to root alyour

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/src/components/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-dvh">
       <Navbar />
       <div className="flex flex-1">
         <Sidebar />
